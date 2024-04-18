3b49d32677389cb2c890aa06d0f11e9b08c134e4
Olcay Tapan <85984966+Olcaytp@users.noreply.github.com>
2024-04-18 11:54:22 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "*21Erkancubicl",
+  password: "**********",
   database: "test",
 });
 
